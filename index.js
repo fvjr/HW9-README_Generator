e0cdf2af28075a5651b94c036eb2327f4721103c
Fernando Vasquez <vasquezjrf@gmail.com>
2022-01-31 20:12:45 -0500

    started array of questions, created init, created function to write input to md file

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's title?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: "What is your project's description?",
+  },
+];
+
+const promptUser = () => {
+  return inquirer.prompt(questions);
+}
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+const writeToFile = ({ title, description}) => 
+`# ${title}
+
+## ${description}`;
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+  promptUser()
+.then((answers) => fs.writeFileSync('GENREADME.md', writeToFile(answers)))
+.then(() => console.log('Successfully wrote to GENREADME.md'))
+.catch((err) => console.error(err));
+
+}
 
 // Function call to initialize app
 init();
